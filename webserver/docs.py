bb5cbf843f2612429ab69c55d6fa0b245a392093
Daryl Weir <darylweir@gmail.com>
2012-03-31 01:20:21 +0100

    Added file zipping

--- webserver/docs.py
+++ webserver/docs.py
@@ -3,6 +3,7 @@ from bottle_sqlite import SQLitePlugin
 # Include the Dropbox SDK libraries
 from dropbox import client, rest, session
 import sqlite3, cPickle
+import zipfile, StringIO
 
 # Get your app key and secret from the Dropbox developer website
 APP_KEY = 'gkucvfe9xptshud'
@@ -70,5 +71,68 @@ def putDoc(docname, template=None):
 		f = file(docname+'.tex','w')
 		cl.put_file('/'+docname+'/'+docname+'.tex', f)
 
+def zip_files(cl, zp, path):
+	md = cl.metadata(path)
+	contents = md['contents']
+	for i in range(len(contents)):
+		temp = contents[i]
+		if bool(temp['is_dir']):
+			zip_files(cl,zp,temp['path'])
+		else:
+			f = cl.get_file(temp['path'])
+			pos = temp['path'].rfind('/')
+			fname = temp['path'][pos+1:]
+			print fname
+			content = f.read()
+			print content, type(content)
+			zp.append(fname,content)
+
+
+
+
+@route('/testzip')
+def ziptest():
+	token = request.get_cookie("access_token",secret="secretkey")
+	if token:
+		sess.set_token(token.key,token.secret)
+		cl = client.DropboxClient(sess)
+		path = '/doc1'
+		myzip = InMemoryZip()
+		zip_files(cl,myzip,path)
+		myzip.writetofile('stuff.zip')
+	
+
 def templateList():
 	pass
+
+class InMemoryZip(object):
+   def __init__(self):
+       # Create the in-memory file-like object
+       self.in_memory_zip = StringIO.StringIO()
+      
+   def append(self, filename_in_zip, file_contents):
+       '''Appends a file with name filename_in_zip and contents of
+          file_contents to the in-memory zip.'''
+       # Get a handle to the in-memory zip in append mode
+       zf = zipfile.ZipFile(self.in_memory_zip, "a", zipfile.ZIP_DEFLATED)
+      
+       # Write the file to the in-memory zip
+       zf.writestr(filename_in_zip, file_contents)
+      
+       # Mark the files as having been created on Windows so that
+       # Unix permissions are not inferred as 0000
+       for zfile in zf.filelist:
+           zfile.create_system = 0       
+      
+       return self
+      
+   def read(self):
+       '''Returns a string with the contents of the in-memory zip.'''
+       self.in_memory_zip.seek(0)
+       return self.in_memory_zip.getvalue()
+  
+   def writetofile(self, filename):
+       '''Writes the in-memory zip to a file.'''
+       f = file(filename, "wb")
+       f.write(self.read())
+       f.close()
